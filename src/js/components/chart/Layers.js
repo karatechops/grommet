8079fb66fd459de5684be2f06e2baf907d0e7d99
Eric Soderberg <eric.soderberg@hp.com>
2016-07-19 13:33:09 -0700

    Layers now always sets the width and height of its children.

--- src/js/components/chart/Layers.js
+++ src/js/components/chart/Layers.js
@@ -1,6 +1,6 @@
 // (C) Copyright 2016 Hewlett Packard Enterprise Development LP
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component, Children, PropTypes } from 'react';
 import CSSClassnames from '../../utils/CSSClassnames';
 
 const CLASS_ROOT = CSSClassnames.CHART_LAYERS;
@@ -9,26 +9,22 @@ export default class Layers extends Component {
 
   render () {
     const { height, width } = this.props;
-    let classes = [CLASS_ROOT];
+
     let style = {...this.props.style};
     if (height) {
-      if (typeof height === 'string') {
-        classes.push(`${CLASS_ROOT}--height-${height}`);
-      } else {
-        style.height = `${height}px`;
-      }
+      style.height = `${height}px`;
     }
     if (width) {
-      if (typeof width === 'string') {
-        classes.push(`${CLASS_ROOT}--width-${width}`);
-      } else {
-        style.width = `${width}px`;
-      }
+      style.width = `${width}px`;
     }
 
+    let children = Children.map(this.props.children, child => {
+      return React.cloneElement(child, { width: width, height: height });
+    });
+
     return (
-      <div className={classes.join(' ')} style={style}>
-        {this.props.children}
+      <div className={CLASS_ROOT} style={style}>
+        {children}
       </div>
     );
   }
@@ -36,12 +32,6 @@ export default class Layers extends Component {
 };
 
 Layers.propTypes = {
-  height: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.oneOf(['small', 'medium', 'large'])
-  ]),
-  width: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.oneOf(['small', 'medium', 'large', 'full'])
-  ])
+  height: PropTypes.number,
+  width: PropTypes.number
 };
